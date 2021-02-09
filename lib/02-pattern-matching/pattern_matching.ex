261e52475906d84fe519c475d1e23eb173167716
Milthon Laureano Ceballos <mlceballos@personalsoft.com.co>
2021-02-09 17:04:55 -0500

    pattern matching

---
+++ lib/02-pattern-matching/pattern_matching.ex
@@ -0,0 +1,10 @@
+IO.puts "Hola mundo"
+[a|b] = mylist = [1,2,3]
+#IO.puts mylist
+IO.inspect(mylist, label: "->")
+IO.inspect(a, label: "->")
+IO.inspect(b, label: "->")
+
+[ %{firtname: a}|b] = personas = [%{firtname: "Milthon", lastname: "Ceballos"},
+                                 %{firtname: "Nicole", lastname: "Ceballos"}]
+IO.inspect(a, label: "-->")
--- lib/curso_elixir.ex
+++ lib/curso_elixir.ex
@@ -1,18 +1,72 @@
 defmodule CursoElixir do
-  @moduledoc """
-  Documentation for `CursoElixir`.
-  """
+  @personas [%{firstname: "Milthon", lastname: "Ceballos"},
+             %{firstname: "Nicole", lastname: "Ceballos"},
+             %{firstname: "Adriana", lastname: "Castaño"}]
 
-  @doc """
-  Hello world.
+  @numeros [1,2,3,4,5,6,7,8]
 
-  ## Examples
+  #buscar
+  def buscar([]), do: "No hay mas items"
 
-      iex> CursoElixir.hello()
-      :world
+  def buscar([%{firstname: firstname, lastname: lastname} | tail] = list \\ @personas) do
+    IO.inspect(tail, label: "-->")
+    IO.puts "Hola mi nombre es #{firstname} #{lastname}"
+    buscar(tail)
+  end
 
-  """
-  def hello do
-    :world
+  #sumar
+  def sumar([], suma ), do: suma
+
+  def sumar([head | tail] \\ @numeros, suma \\ 0) do
+    resultado = suma + head
+    sumar(tail, resultado)
+  end
+
+
+  #generico
+  def generico([], suma, _funcion ), do: suma
+
+  def generico([head | tail] \\ @numeros, suma \\ 0, funcion) do
+    resultado = funcion.(suma , head)
+    generico(tail, resultado, funcion)
+  end
+
+
+  #bucarNombres
+  def buscarNombres() do
+    Enum.map(@personas, fn (key) -> "Hola mi nombre #{key.firstname} y mi apellido es #{key.lastname}" end)
+  end
+
+ #buscarNumero
+  def buscarNumero([], _), do: false
+
+  def buscarNumero([head | tail], numero) do
+    if head == numero do
+      IO.puts(true)
+    else
+      buscarNumero(tail, numero);
+    end
+  end
+
+  #contar cantidad elementos de una lista
+  def contar([], count), do: "La lista tiene #{count} elementos."
+
+  def contar([_ | tail], count) do
+    count = count + 1
+    contar(tail, count)
+  end
+
+
+
+  #concatenar todos los elementos de una lista
+  def concatenar_elementos([], concat), do: {:ok, concat}
+
+  def concatenar_elementos([head | _tail], _concat) when is_number(head) do
+    {:error, "Hay un numero en la lista."}
+  end
+
+  def concatenar_elementos([head | tail], concat \\ "") when is_binary(head) do
+    concat = concat <> head
+    concatenar_elementos(tail, concat)
   end
 end
