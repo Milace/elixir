aed676dd8821758e937c98851f17cd474bd6fc36
Milthon Laureano Ceballos <mlceballos@personalsoft.com.co>
2021-02-11 14:22:04 -0500

    se añaden ejercicios

--- lib/curso_elixir.ex
+++ lib/curso_elixir.ex
@@ -97,4 +97,52 @@ defmodule CursoElixir do
       fibonacci(n - 2) + fibonacci(n - 1)
     end
   end
+
+  #Ejercicio #1
+  def ejer1(0, sum), do: sum
+
+  def ejer1(n, sum \\ 0) do
+    sum = sum + n
+    ejer1(n-1, sum)
+  end
+
+  def ejer11(n) do
+    Enum.sum(0..n)
+  end
+
+  #Ejercicio #2
+  def ejer2(n) do
+    if n > 0 do
+      Enum.map(1..n, &(&1))
+    end
+  end
+
+  def ejer21(n) do
+    fibo = 
+    cond do
+      n > 2 -> fibonacci(n-1) + fibonacci(n-2)
+      n == 1 or n == 2 -> 1
+    end
+  end
+
+  #Ejercicio #3
+  def ejer3(a,b) do
+    result = 
+    cond do
+      a == 0 or b == 0 -> 0
+      a == b -> ejer3(a - 1, b - 1) + 2 * a - 1
+      a > b  -> ejer3(a - b, b) + ejer3(b, b)
+      a < b  -> ejer3(a, a) + ejer3(b - a, a)
+    end
+  end
+
+  #Ejercicio #4
+  def ejer4(a,b) do
+    result = 
+    cond do
+      b == 0 -> a
+      b > a -> ejer4(b, a)
+      true -> ejer4(a - b, b)
+    end
+  end
 end
